6b8263059a17512c62e9333e0d442041e89cce96
Karneyaprojects <karneya.r02@gmail.com>
2025-09-17 08:25:51 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-
-// Railway backend URL (fallback to localhost in dev)
-// const backendUrl ="http://deeplearner-production.up.railway.app";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": 
-
-"http://deeplearner-production.up.railway.app",
-    }
-  },
 });
